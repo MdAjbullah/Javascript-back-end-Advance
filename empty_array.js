056724565c0fe531caa8fd73b295be9b917d5bbd
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-02-06 01:44:44 +0530

    write a progrma  empty array

---
+++ empty_array.js
@@ -0,0 +1,7 @@
+// write a progrma  empty array in javascript don not rset it to a new array and do not loop through tp pop each value
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+arr.length = 0;
+console.log(arr); //[]
